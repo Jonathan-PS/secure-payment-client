b065445d4936f04ef7ec6faa6c864d561abf7055
kaaasen <kim.andre.aasen@gmail.com>
2019-04-25 18:42:24 +0200

    Adjusted Col-values

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import './LandingPage.css';
-import NavbarCollapse from 'react-bootstrap/NavbarCollapse';
 
 class LandingPage extends React.Component {
     constructor(props, context) {
@@ -43,55 +41,6 @@ class LandingPage extends React.Component {
                     <h5>Feel free to browse our goods.</h5>
                     <Button href="/products" variant="primary">Go to Products</Button>
                 </Jumbotron>
-
-
-                {/* 
-                <Carousel
-                    id="carousel"
-                    activeIndex={index}
-                    direction={direction}
-                    onSelect={this.onSelect}
-                >
-                    <Carousel.Item>
-                        <h1>NOW IN STOCK!</h1>
-                        <img id="test"
-                            className="d-block w-100"
-                            src="http://sweetclipart.com/multisite/sweetclipart/files/baby_boy_socks_blue.png"
-                            alt="Blue Baby Socks"
-                        />
-                        <Carousel.Caption>
-                            <h2>Blue Baby Socks</h2>
-                            <p>Nice pair of blue socks for baby boy</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <h1>NOW IN STOCK!</h1>
-                        <img
-                            className="d-block w-100"
-                            src="http://sweetclipart.com/multisite/sweetclipart/files/baby_girl_socks_pink.png"
-                            alt="Pink Baby Socks"
-                        />
-
-                        <Carousel.Caption>
-                            <h2>Pink Baby Socks</h2>
-                            <p>Nice pair of pink socks for baby girl</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <h1>NOW IN STOCK!</h1>
-                        <img
-                            className="d-block w-100"
-                            src="http://sweetclipart.com/multisite/sweetclipart/files/baby_socks_yellow.png"
-                            alt="Third slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h2>Yellow Baby Socks</h2>
-                            <p>Nice pair of yellow socks</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-                */}
             </div>
         );
     }
