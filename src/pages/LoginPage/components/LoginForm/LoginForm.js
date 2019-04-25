02531798510b21497f1a1da0893fe78edd97b588
kaaasen <kim.andre.aasen@gmail.com>
2019-04-25 18:15:42 +0200

    Made all fields required, removed primitive validateForm-method in both files

--- src/pages/LoginPage/components/LoginForm/LoginForm.js
+++ src/pages/LoginPage/components/LoginForm/LoginForm.js
@@ -9,11 +9,6 @@ class LoginForm extends Component {
     password: ""
   };
 
-  /* Checks whether email and password are typed in at all */
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
-  }
-
   /* Handles user inputs into the fields of email and password. */
   handleChange = event => {
     this.setState({
@@ -67,6 +62,7 @@ class LoginForm extends Component {
                 type="email"
                 value={this.state.email}
                 onChange={this.handleChange}
+                required="required"
               />
             </FormGroup>
             <FormGroup controlId="password">
@@ -75,11 +71,11 @@ class LoginForm extends Component {
                 value={this.state.password}
                 onChange={this.handleChange}
                 type="password"
+                required="required"
               />
             </FormGroup>
             <Button
               block
-              disabled={!this.validateForm()}
               type="submit"
               variant="dark"
             >
